6e4d18381b5b1d89641991de900da7e315280834
deividsberkolds <deivids.berkolds@gmail.com>
2020-03-04 00:08:59 +0200

    before flux

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,20 @@ import CoursesPage from './CoursesPage';
 import NotFoundPage from './NotFoundPage';
 import ManageCoursePage from './ManageCoursePage';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="container-fluid">
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" exact component={CoursesPage} />
         <Route path="/about" exact component={AboutPage} />
         <Route path="/course/:slug" exact component={ManageCoursePage} />
+        <Route path="/course" exact component={ManageCoursePage} />
         <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
